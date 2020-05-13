e00e46c9b46afef1658a344e630b0ba6a955ac16
Ubuntu <ubuntu@ip-172-31-31-144.us-east-2.compute.internal>
2020-05-13 21:02:13 +0000

    first commit

---
+++ home_js.js
@@ -0,0 +1,77 @@
+// JavaScript File
+/*global$*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadCovid19Data1("https://api.covid19api.com/country/romania/status/confirmed");
+    loadCovid19Data2("https://api.apify.com/v2/datasets/n1XtXTelVG5dJhDhy/items?format=json&clean=1");
+});
+
+function loadCovid19Data1(url) {
+    function manipulateDom(response) {
+        for(const row of response) {
+            insertTrToApi1Table(row);
+        }
+    }
+    
+    getDataFromRequest(url, manipulateDom);
+}
+
+function loadCovid19Data2(url) {
+    function manipulateDom(response) {
+        for(const row of response) {
+            insertTrToApi2Table(row);
+        }
+    }
+    
+    getDataFromRequest(url, manipulateDom);
+}
+
+function getDataFromRequest(url, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(JSON.parse(xhttp.response));
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function insertTrToApi1Table(rowData) {
+    const tbody = document.querySelector('#api1_table .api_body');
+    let tr = document.createElement('div');
+    tr.classList.add('api_row');
+    let parsedDate =  new Date(rowData.Date);
+    const ye = new Intl.DateTimeFormat('ro', { year: 'numeric' }).format(parsedDate);
+    const mo = new Intl.DateTimeFormat('ro', { month: 'short' }).format(parsedDate);
+    const da = new Intl.DateTimeFormat('ro', { day: '2-digit' }).format(parsedDate);
+    const formattedDate = da + ' ' + mo + ' ' + ye;
+    
+    tr.innerHTML = `
+        <div>${rowData.CountryCode}</div>
+        <div>${formattedDate}</div>
+        <div>${rowData.Cases}</div>
+        <div>${rowData.Status}</div>
+    `;
+    tbody.append(tr);
+}
+function insertTrToApi2Table(rowData) {
+   // console.log(rowData);
+    const tbody = document.querySelector('#api2_table .api_body');
+    let tr = document.createElement('div');
+    tr.classList.add('api_row');
+    let parsedDate =  new Date(rowData.lastUpdatedAtSource);
+    const ye = new Intl.DateTimeFormat('ro', { year: 'numeric' }).format(parsedDate);
+    const mo = new Intl.DateTimeFormat('ro', { month: 'short' }).format(parsedDate);
+    const da = new Intl.DateTimeFormat('ro', { day: '2-digit' }).format(parsedDate);
+    const formattedDate = da + ' ' + mo + ' ' + ye;
+    
+    tr.innerHTML = `
+        <div>${formattedDate}</div>
+        <div>${rowData.tested}</div>
+        <div>${rowData.infected}</div>
+        <div>${rowData.deceased}</div>
+        <a href="${rowData.sourceUrl}">link</a>
+    `;
+    tbody.append(tr);
+}
